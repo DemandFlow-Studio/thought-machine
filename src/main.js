1d9c2670197a82eb3bc4806b667e2f488897c016
hishaamfataar <hishaamfataar@gmail.com>
2026-07-07 12:57:48 +0200

    added copy to clipboard

--- src/main.js
+++ src/main.js
@@ -2601,6 +2601,53 @@ function initSmooothy() {
     snap: false,
   });
 }
+
+function initCopyEmailClipboard() {
+  const buttons = document.querySelectorAll('.copy-email-button');
+  if (!buttons.length) return;
+
+  const copyEmail = (button) => {
+  	// Email to copy to clipboard is taking from the button itself, or if that's empty,
+    // from a text element inside the button
+    const email =
+      button.getAttribute('data-copy-email') ||
+      button.querySelector('[data-copy-email-element]').textContent.trim();
+    if (email) {
+      navigator.clipboard.writeText(email).then(() => {
+        button.setAttribute('data-copy-button', 'copied');
+        button.setAttribute('aria-label', 'Email copied to clipboard!');
+      });
+    }
+  };
+
+  const handleInteraction = (e) => {
+    if (
+      e.type === 'click' ||
+      (e.type === 'keydown' && (e.key === 'Enter' || e.key === ' '))
+    ) {
+      e.preventDefault();
+      copyEmail(e.currentTarget);
+    }
+  };
+
+  buttons.forEach((button) => {
+    button.addEventListener('click', handleInteraction);
+    button.addEventListener('keydown', handleInteraction);
+    button.addEventListener('mouseleave', () => {
+    	// Remove 'active' attribute to reset color and text transform
+      button.removeAttribute('data-copy-button');
+      // Remove focus on mouseleave to clear keyboard focus styling
+      button.blur();
+      button.setAttribute('aria-label', 'Copy email to clipboard');
+    });
+    button.addEventListener('blur', () => {
+      button.removeAttribute('data-copy-button');
+      button.setAttribute('aria-label', 'Copy email to clipboard');
+    });
+  });
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
   // visual, font-independent — run immediately
   initButton046();
@@ -2617,6 +2664,8 @@ document.addEventListener('DOMContentLoaded', () => {
     initAnimatedBackground();
       initToggleSwitches();
       initSmooothy();
+        initCopyEmailClipboard();
+
 
 
 
