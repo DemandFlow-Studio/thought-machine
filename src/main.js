468466e35d10134cc894f1ed6c752dd8967ffa4a
Hishaam Fataar <hishaam@demandflow.co>
2026-08-19 20:30:51 +0200

    made case studies carousel swiper responsive

--- src/main.js
+++ src/main.js
@@ -2250,10 +2250,15 @@ function initSwipers() {
           modules: [Keyboard, Mousewheel],
           speed: 600,
           spaceBetween: 0,
-          slidesPerView: 2.25,
+          slidesPerView: 1,
           centeredSlides: true,
           loop: true,
           grabCursor: true,
+          breakpoints: {
+            1024: {
+              slidesPerView: 2.25,
+            }
+          },
           keyboard: {
             enabled: true,
           },
